2805e648ab3ff374a9485cf32de30a648a855bb3
hoyeungkim <youniline1214@gmail.com>
2025-12-22 04:20:38 +0900

    [Feat] solution making

--- core/config.py
+++ core/config.py
@@ -6,6 +6,7 @@ load_dotenv()
 MONGO_URL = os.getenv("MONGO_URL")
 KAKAO_API_KEY = os.getenv("KAKAO_API")
 DATA_GO_KR_API_KEY = os.getenv("DATA_GO_KR_API_KEY")
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
 #await 붙여야함- 비동기 실행
 client = AsyncIOMotorClient(MONGO_URL)
@@ -16,3 +17,5 @@ db = client["BIZIT_DB"]
 #나중에 쓸때는 await users_collection.function(...)
 user_collection = db["users"]
 store_collection = db['storeInfo']
+solution_collection = db['solutionInfo']
+surrounding_collection = db['surroundingInfo']
